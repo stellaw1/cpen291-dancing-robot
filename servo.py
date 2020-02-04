71a23df8df31b580cd9d927cbab46c72388aee40
manek11 <54968278+manek11@users.noreply.github.com>
2020-02-04 10:28:58 -0800

    Update servo.py

--- servo.py
+++ servo.py
@@ -45,3 +45,29 @@ while True:
     print("stop")
     my_servo.throttle = 0.0
     time.sleep(4.0)
+#Working code use A1
+#Continuous Servo Code
+import time
+import board
+import pulseio
+from adafruit_motor import servo
+ 
+# create a PWMOut object on Pin A2.
+pwm = pulseio.PWMOut(board.A1, frequency=50)
+ 
+# Create a servo object, my_servo.
+my_servo = servo.ContinuousServo(pwm)
+ 
+while True:
+    print("forward")
+    my_servo.throttle = 1.0
+    time.sleep(2.0)
+    print("stop")
+    my_servo.throttle = 0.1
+    time.sleep(2.0)
+    print("reverse")
+    my_servo.throttle = -1.0
+    time.sleep(2.0)
+    print("stop")
+    my_servo.throttle = 0.1
+    time.sleep(4.0)
