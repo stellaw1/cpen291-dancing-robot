381b7d267941ce709e75e6a66a05af736fedab6b
manek11 <manekgujral11@gmail.com>
2020-02-04 06:01:33 -0800

    Servo starter code added

---
+++ servo.py
@@ -0,0 +1,47 @@
+#Standard Sevo Code (-90 to 90)
+import time
+import board
+import pulseio
+from adafruit_motor import servo
+ 
+# create a PWMOut object on Pin A2.
+pwm = pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+ 
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+ 
+while True:
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+
+#############################################
+
+#Continuous Servo Code
+import time
+import board
+import pulseio
+from adafruit_motor import servo
+ 
+# create a PWMOut object on Pin A2.
+pwm = pulseio.PWMOut(board.A2, frequency=50)
+ 
+# Create a servo object, my_servo.
+my_servo = servo.ContinuousServo(pwm)
+ 
+while True:
+    print("forward")
+    my_servo.throttle = 1.0
+    time.sleep(2.0)
+    print("stop")
+    my_servo.throttle = 0.0
+    time.sleep(2.0)
+    print("reverse")
+    my_servo.throttle = -1.0
+    time.sleep(2.0)
+    print("stop")
+    my_servo.throttle = 0.0
+    time.sleep(4.0)
