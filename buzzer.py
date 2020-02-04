3c70d1f760d92e1c98ed2c264f82e9133187a310
Amir Barkam <amirbarkam6@gmail.com>
2020-02-04 09:14:14 -0800

    Add buzzer code

---
+++ buzzer.py
@@ -0,0 +1,20 @@
+# Starter code taken from https://learn.adafruit.com/introducing-adafruit-itsybitsy-m4/circuitpython-pwm
+
+import time
+import board
+import pulseio
+ 
+# For the M0 boards:
+piezo = pulseio.PWMOut(board.A2, duty_cycle=0, frequency=440, variable_frequency=True)
+ 
+# For the M4 boards:
+# piezo = pulseio.PWMOut(board.A1, duty_cycle=0, frequency=440, variable_frequency=True)
+ 
+while True:
+    for f in (262, 294, 330, 349, 392, 440, 494, 523):
+        piezo.frequency = f
+        piezo.duty_cycle = 65536 // 2  # On 50%
+        time.sleep(0.25)  # On for 1/4 second
+        piezo.duty_cycle = 0  # Off
+        time.sleep(0.05)  # Pause between notes
+    time.sleep(0.5)
