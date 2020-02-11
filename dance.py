209239a9b733ab405020f7ad7453642f0cb11e72
Amir Barkam <amirbarkam6@gmail.com>
2020-02-11 08:02:58 -0800

    Temporary dance moves

--- dance.py
+++ dance.py
@@ -3,52 +3,72 @@ import board
 import pulseio
 from adafruit_motor import servo
  
-pwm1 = pulseio.PWMOut(board.A1, frequency=50) #leg1
-my_servo1 = servo.ContinuousServo(pwm1)
+pwm1 = pulseio.PWMOut(board.D10, frequency=50)
+right_leg = servo.ContinuousServo(pwm1)
 
-pwm2 = pulseio.PWMOut(board.A3, frequency=50) #leg2
-my_servo2 = servo.ContinuousServo(pwm2)
+pwm2 = pulseio.PWMOut(board.D11, frequency=50)
+left_leg = servo.ContinuousServo(pwm2)
 
-pwm3 = pulseio.PWMOut(board.A4, frequency=50)
-my_servo3 = servo.ContinuousServo(pwm3)
+pwm3 = pulseio.PWMOut(board.D12, frequency=50)
+right_foot = servo.ContinuousServo(pwm3)
 
-pwm4 = pulseio.PWMOut(board.A5, frequency=50)
-my_servo4 = servo.ContinuousServo(pwm4)
+pwm4 = pulseio.PWMOut(board.D13, frequency=50)
+left_foot = servo.ContinuousServo(pwm4)
  
 # define basic functions 
 def rightShuffle():
     print("shuffling")
     for i in range(3):
-        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
+        for angle in range(0, 90, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            right_leg.angle = angle
             time.sleep(0.05)
 
 def leftShuffle():
     print("shuffling")
     for i in range(3):
-        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
+        for angle in range(90, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            left_leg.angle = angle
             time.sleep(0.05)
 
 def rightKick():
     print("right kick")
     for i in range(3):
-        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
+        for angle in range(0, 90, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            right_foot.angle = angle
+            time.sleep(0.05)
+        time.sleep(1)
+        for angle in range(0, 90, 5):
+            right_foot.angle = 90 - angle
             time.sleep(0.05)
 
 def leftKick():
     print("right kick")
     for i in range(3):
-        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
+        for angle in range(0, 90, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            left_foot.angle = angle
+            time.sleep(0.05)
+        time.sleep(1)
+        for angle in range(0, 90, 5):
+            left_foot.angle = 90 - angle
+            time.sleep(0.05)
+
+def tiptoes():
+    for i in range(3):
+        for angle in range(0, 20, 5):
+            left_foot.angle = angle
+            right_foot.angle = angle + 90
+            time.sleep(0.05)
+        time.sleep(1)
+        for angle in range(0, 20, 5):
+            left_foot.angle = 20 - angle
+            right_foot.angle = 110 - angle
             time.sleep(0.05)
 
 def rightStep():
     print("right kick")
     for i in range(3):
-        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
+        for angle in range(90, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            right_foot.angle = angle
             time.sleep(0.05)
 
 def leftStep():
