5dd6653b1c2a923ea80220fcd1739ab6d67476ec
stellaw1 <stella01wang@gmail.com>
2020-02-18 11:15:52 -0800

    finished 6 main dance moves

--- dance_dutycycle.py
+++ dance_dutycycle.py
@@ -19,27 +19,28 @@ footR = servo.Servo(pwm4)
 
 
 ###################################
-# define basic dance move functions 
+# define basic functions
 def reset(): 
     legR.angle = 94
     legL.angle = 90
     footR.angle = 90
     footL.angle = 91
 
+def rotate(limb, min, max, step):
+    for x in range(min, max, step):
+        limb.angle = x
+
+# define single dance move functions 
 def leftFootOut():
     reset()
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        legL.angle = angle
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        legL.angle = angle
+    rotate(legL, 90, 180, 5)
+    rotate(legL, 180, 90, -5)
     reset()
 
 def rightFootOut():
     reset()
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        legR.angle = angle
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        legR.angle = angle
+    rotate(legR, 90, 10, -5)
+    rotate(legR, 10, 90, 5)
     reset()
 
 def twist(): 
@@ -64,41 +65,98 @@ def jump():
 
 def rightKick():
     legR.angle = 20
-    for angle in range(90, 140, 4):  # 0 - 180 degrees, 5 degrees at a time.
-        footR.angle = angle
-    for angle in range(140, 90, -4): # 180 - 0 degrees, 5 degrees at a time.
-        footR.angle = angle
+    rotate(footR, 90, 130, 4)
+    rotate(footR, 130, 90, -4)
     reset()
 
 def leftKick():
     legL.angle = 160
-    for angle in range(90, 60, -3):  # 0 - 180 degrees, 5 degrees at a time.
-        footL.angle = angle
-    for angle in range(60, 90, 3): # 180 - 0 degrees, 5 degrees at a time.
-        footL.angle = angle
+    rotate(footL, 90, 60, -3)
+    rotate(footL, 60, 90, 3)
     reset()
 
 def shuffle():
-    reset()
-    for i in range(8):
-        for angle in range(90, 60, -5):  # 0 - 180 degrees, 5 degrees at a time.
-            legL.angle = angle
-            legR.angle = 90 + (angle - 60)
-        for angle in range(60, 90, 5): # 180 - 0 degrees, 5 degrees at a time.
-            legL.angle = angle
-            legR.angle = 90 + (angle - 60)
+    for angle in range(90, 60, -5):  # 0 - 180 degrees, 5 degrees at a time.
+        legL.angle = angle
+        legR.angle = 90 + (angle - 60)
+    for angle in range(60, 90, 5): # 180 - 0 degrees, 5 degrees at a time.
+        legL.angle = angle
+        legR.angle = 90 + (angle - 60)
     reset()    
 
-def slide():
+def wiggle():
+    rotate(footL, 90, 130, 5)
+    rotate(footR, 90, 60, -5)
+    rotate(footL, 130, 90, -5)
+    rotate(footR, 60, 90, 5)
+
+
+def tapLeftFoot():
+    reset()
+    rotate(footL, 90, 60, -3)
+    rotate(footL, 60, 90, 3)
+    reset()
+    
+def tapRightFoot():
+    reset()
+    rotate(footR, 90, 120, 3)
+    rotate(footR, 120, 90, -3)
+    reset()
+ 
+def tapBothFeet():
+    reset()
+    for angle in range(90, 130, 8):  # 0 - 180 degrees, 5 degrees at a time.
+        footL.angle = 90 - (angle - 90)
+        footR.angle = angle
+    for angle in range(130, 90, -8): # 180 - 0 degrees, 5 degrees at a time.
+        footL.angle = 90 - (angle - 90)
+        footR.angle = angle
+    reset()
+
+
+
+# define 6 main dance moves
+
+#1: slide
+def dance1():
     reset()
     for i in range(8):
-        for angle in range(90, 130, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            footL.angle = angle
-        for angle in range(90, 60, -5):
-            footR.angle = angle
-        for angle in range(130, 90, -5): # 180 - 0 degrees, 5 degrees at a time.
-            footL.angle = angle
-        for angle in range(60, 90, 5):
-            footR.angle = angle
-    reset() 
+        wiggle()
+    reset()
 
+#2: line dance
+def dance2():
+    for i  in range(2):
+        leftFootOut()
+        rightFootOut()
+        twist()
+
+#3: karate kick
+def dance3():
+    leftFootOut()
+    tapLeftFoot()
+    leftKick()
+
+    rightFootOut()
+    tapRightFoot()
+    rightKick()
+
+#4: tap feet to the beat
+def dance4():
+    for i in range(4):       
+        tapLeftFoot()
+    for i in range(4):       
+        tapRightFoot()
+    for i in range(4):       
+        tapBothFeet()
+
+#5: walk
+def dance5():
+    for i in range(5):
+        leftKick()
+        rightKick()
+
+#6: 
+def dance6():
+    for i in range(8):
+        shuffle()
