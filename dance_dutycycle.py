ab9b8d0981e9754ef295fc84e99e0d133da44f15
stellaw1 <stella01wang@gmail.com>
2020-02-18 11:44:04 -0800

    finished adding songs to dance moves

--- dance_dutycycle.py
+++ dance_dutycycle.py
@@ -17,6 +17,10 @@ footL = servo.Servo(pwm3)
 pwm4 = pulseio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency=50)
 footR = servo.Servo(pwm4)
 
+#buzzer setup
+piezo = pulseio.PWMOut(board.A1 , duty_cycle=0, frequency=440, variable_frequency=True)
+
+
 
 ###################################
 # define basic functions
@@ -32,26 +36,20 @@ def rotate(limb, min, max, step):
 
 # define single dance move functions 
 def leftFootOut():
-    reset()
     rotate(legL, 90, 180, 5)
     rotate(legL, 180, 90, -5)
-    reset()
 
 def rightFootOut():
-    reset()
     rotate(legR, 90, 10, -5)
     rotate(legR, 10, 90, 5)
-    reset()
+    
+def leftFootIn():
+    rotate(legL, 90, 20, -5)
+    rotate(legL, 20, 90, 5)
 
-def twist(): 
-    reset()
-    for angle in range(10, 180, 10):  # 0 - 180 degrees, 5 degrees at a time.
-        legL.angle = angle
-        legR.angle = angle
-    for angle in range(180, 10, -10): # 180 - 0 degrees, 5 degrees at a time.
-        legL.angle = angle
-        legR.angle = angle
-    reset()
+def rightFootIn():
+    rotate(legR, 90, 160, 5)
+    rotate(legR, 160, 90, -5)
 
 def jump():
     reset()
@@ -115,48 +113,105 @@ def tapBothFeet():
 
 
 
+####################################################
+# song frequency arrays
+ANTHEM = [196, 277, 196, 220, 247, 165, 165, 233, 196, 174, 208, 131, 131, 156, 147, 165, 185, 174, 196, 233, 123,
+            262, 311, 311, 196, 330, 294, 261, 311, 247, 196, 277, 247, 220, 247, 165, 165, 233, 196, 131, 131, 277,
+            247, 220, 207, 207, 207]
+
+MARIO = [330, 330, 330, 262, 330, 392, 196, 262, 196, 165, 220, 247, 233, 220, 196, 330, 392, 440, 349, 392, 330, 
+            262, 294, 247]
+
+CRIMSON = [196, 247, 294, 370, 392, 370, 294, 247, 196, 262, 294, 392, 294]
+
+CANON = [131, 165, 196, 262, 98, 123, 147, 196, 110, 131, 165, 220, 82, 98, 123, 165, 87, 110, 131, 175, 
+            131, 165, 196, 262, 87, 110, 131, 175, 98, 123, 147, 196, 110]
+
+TETRIS = [659, 494, 523, 587, 659, 587, 523, 494, 440, 440, 523, 659, 587, 523, 494, 494, 494, 523, 587, 523,
+            494, 494, 494, 523, 587, 659, 523, 440, 440, 587, 587, 698, 880, 784, 698, 659, 659, 523, 659, 587, 
+            523, 494, 494, 523, 587, 659, 523, 440, 440, 659, 494, 523, 587, 659, 587, 523, 494, 440, 440, 523, 
+            659, 587, 523, 494, 494, 523, 587, 659, 523, 440, 440, 587, 587, 698, 880, 784, 698, 659, 659, 523,
+            659, 587, 523, 587, 659, 523, 440, 440]
+
+DEFAULT = [149, 149, 149, 446, 1485, 149, 149, 149, 446, 297, 297, 149, 595, 149, 149, 149, 149, 1931]
+
+
+###################################
+# define buzzer song functions
+def play_note(freq):
+    piezo.frequency = freq
+    piezo.duty_cycle = 65536 // 2  # On 50%
+    time.sleep(0.3) # On for 1/4 second
+    piezo.duty_cycle = 0 # Off
+
+
+
+
+####################################################
 # define 6 main dance moves
 
-#1: slide
+#1: slide to intro to All I Want for Christmas is You - Mariah Carey
 def dance1():
     reset()
-    for i in range(8):
+    for i in range(0, 12, 1):
+        play_note(CRIMSON[i])
         wiggle()
     reset()
 
-#2: line dance
+
+#2: line dance to the MARIO THEME song
 def dance2():
-    for i  in range(2):
+    for i in range(0, len(MARIO), 4):
+        play_note(MARIO[i])
         leftFootOut()
+        play_note(MARIO[i+1])
+        leftFootIn()
+        play_note(MARIO[i+2])
         rightFootOut()
-        twist()
+        play_note(MARIO[i+3])
+        rightFootIn()
 
-#3: karate kick
+
+#3: karate kick to the USSR ANTHEM
 def dance3():
-    leftFootOut()
-    tapLeftFoot()
-    leftKick()
-
-    rightFootOut()
-    tapRightFoot()
-    rightKick()
-
-#4: tap feet to the beat
-def dance4():
-    for i in range(4):       
+    for i in range(0, len(ANTHEM), 6):
+        play_note(ANTHEM[i])
+        leftFootOut()
+        play_note(ANTHEM[i+1])
         tapLeftFoot()
-    for i in range(4):       
-        tapRightFoot()
-    for i in range(4):       
-        tapBothFeet()
-
-#5: walk
-def dance5():
-    for i in range(5):
+        play_note(ANTHEM[i + 2])
         leftKick()
+
+        play_note(ANTHEM[i + 3])
+        rightFootOut()
+        play_note(ANTHEM[i + 4])
+        tapRightFoot()
+        play_note(ANTHEM[i + 5])
         rightKick()
 
-#6: 
+#4: tap feet to the beat of Tetris background music
+def dance4():
+    for i in range(0, len(TETRIS), 12):
+        for j in range(0, 3, 1):
+            play_note(TETRIS[i + j])
+            tapLeftFoot()
+        for j in range(0, 3, 1):  
+            play_note(TETRIS[i + 4 + j])
+            tapRightFoot()
+        for j in range(0, 3, 1):
+            play_note(TETRIS[i + 8 + j])
+            tapBothFeet()
+
+#5: walk to Pachebel Canon in C
+def dance5():
+    for i in range(0, len(CANON), 2):
+        play_note(CANON[i])
+        leftKick()
+        play_note(CANON[i + 1])
+        rightKick()
+
+#6: wiggle to the fortnite default song
 def dance6():
-    for i in range(8):
+    for i in range(0, len(DEFAULT), 2):
+        play_note(DEFAULT[i])
         shuffle()
