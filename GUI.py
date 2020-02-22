d28b83f40168d62ed53cad1a62bf32936031b016
ArnoldYing <ArnoldYing@users.noreply.github.com>
2020-02-21 17:30:44 -0800

    stuff

--- GUI.py
+++ GUI.py
@@ -20,6 +20,24 @@ import servo
 from analogio import AnalogIn
 import adafruit_hcsr04
 
+
+#------------------------------------------------------------------------------------------------------#
+# 
+# sonar code
+#     
+#------------------------------------------------------------------------------------------------------#    
+# sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D4, echo_pin=board.D3)
+# threshold = 0.3
+
+# while True:
+#     try:
+#         print((sonar.distance,))
+#         if sonar.distance < threshold:
+#             print("Detected")
+#     except RuntimeError:
+#         print("Retrying!")
+#     time.sleep(0.1)
+
 #------------------------------------------------------------------------------------------------------#
 # 
 # dance code
